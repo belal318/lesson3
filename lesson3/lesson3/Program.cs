fd0c8e4dc36f23debbe32b8dceeb634b27078416
belal <belal.a75@gmail.com>
2018-10-30 22:28:30 +0200

    init

---
+++ lesson3/lesson3/Program.cs
@@ -0,0 +1,73 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace lesson3
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int i = 2;
+//            Console.WriteLine("Enter a number");
+//            int num = int.Parse(Console.ReadLine());
+
+//            while (num > 1)
+//            {
+//                if (num % i == 0)
+//                {
+//                    Console.WriteLine(i);
+//                }
+
+
+//                else if (i > 10)
+//                {
+//                    break;
+//                }
+//                i++;
+//            }
+
+
+//        }
+//    }
+//}
+
+
+
+
+////-----------------------------------------7BOOOM-------------------------------------------------------
+///
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int max = 1;
+            while (max <= num)
+            {
+                if (max % 7 == 0)
+                {
+                    Console.WriteLine("7 BOOOM");
+                }
+                else
+                {
+                    Console.WriteLine(max);
+                }
+                max++;
+            }
+
+        }
+    }
+}
